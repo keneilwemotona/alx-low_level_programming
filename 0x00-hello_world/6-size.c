dc4d99dda4ca6211586ca9683d34abb1ec8eb6f9
keneilwemotona <keneilwem333@gmail.com>
2023-07-15 13:58:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
+
 	return (0);
 }
