eb79db19faced98476ee58b1280e4797932402d7
keneilwemotona <keneilwem333@gmail.com>
2023-07-17 09:58:56 -0700

    1-last_digir.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0", n, ld);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, ld);
 	}
 	return (0);
 }
