2cd04cd41f5aa5cc89d6b1a98bbcd82830964d47
keneilwemotona <keneilwem333@gmail.com>
2023-07-21 15:05:15 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		num = num;
 	}
 
@@ -19,5 +19,5 @@ void print_number(int n)
 		print_number(num / 10);
 	}
 
-	putchar((num % 10) + '0');
+	_putchar((num % 10) + '0');
 }
